ea1d20a41aa9729d9afe340169b4dae169ddec98
Deepak Acharya <64803000+acdepak@users.noreply.github.com>
2022-01-23 15:20:20 +0545

    DFA start with 010

---
+++ DFA start 010.c
@@ -0,0 +1,56 @@
+//start with 010
+#include<stdio.h>
+enum state {q0,q1,q2,q3,q4};
+enum state transition(enum state, char ch);
+void main()
+{
+    char bstr[20], ch;
+    int i=0;
+    enum state curr_state=q0;
+    printf("Enter binary string:");
+    scanf("%s",bstr);
+    bstr[i];
+    while(ch!='\0')
+    {
+        curr_state=transition(curr_state,ch);
+        ch=bstr[++i];
+    }
+    if(curr_state==q3)
+        printf(" Accepted");
+    else
+        printf(" Rejected ");
+
+}
+enum state transition(enum state s, char ch)
+{
+    enum state new_state;
+    switch(s)
+    {
+        case q0:
+            if(ch=='0')
+                new_state=q1;
+            else
+                new_state=q4;
+            break;
+        case q1:
+            if(ch=='0')
+                new_state=q4;
+            else
+                new_state=q2;
+            break;
+        case q2:
+            if(ch=='0')
+                new_state=q3;
+            else
+                new_state=q4;
+            break;
+        case q3:
+            new_state=q3;
+            break;
+        case q4:
+            new_state=q4;
+            break;
+    }
+    return new_state;
+}
+
